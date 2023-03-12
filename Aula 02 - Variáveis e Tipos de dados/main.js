ec8c5fa3af71e451f9609bee5b6e0d8cd3f96f75
and <andersonvittor1801@gmail.com>
2023-03-12 16:02:11 -0300

    Segunda Aula

---
+++ Aula 02 - Variáveis e Tipos de dados/main.js
@@ -0,0 +1,34 @@
+// Strigs
+
+const message = 'Hello world'
+
+console.log(message.length) // mostra o tamanho da variável
+
+const firstname = 'Anderson'
+const lastname = 'Vitor'
+
+const names = 'Felipe, and, Julia'
+
+console.log('Meu nome é ' + firstname + ' ' + lastname)
+console.log(`Meu nome é ${firstname.toUpperCase()} ${lastname.toLowerCase()}`);
+
+console.log(names.split(",")); // coloca cada nome em uma lista
+
+// NUMBERS
+
+const number = 5;
+
+console.log(number + 10);
+console.log(typeof number);
+
+// BOLEANOS
+
+false 
+true
+
+console.log(2 == 3)
+
+// Null & undefinied
+
+const x = null;
+const y = undefined;
